e6562b070cf758dafc15905df7777d3fc989f74e
jojuchatru1993 <jojuchatru1993@gmail.com>
2025-04-24 11:48:04 -0500

    add new validations for dto

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -12,22 +12,31 @@ import { ValidRoles } from '../../auth/interfaces/valid-roles.interface';
 
 export class CreateUserDto {
   @IsString()
+  @MinLength(3)
+  @MaxLength(50)
   @IsNotEmpty()
   firstName: string;
 
   @IsString()
+  @MinLength(3)
+  @MaxLength(50)
   @IsNotEmpty()
   lastName: string;
 
   @IsEmail()
+  @MaxLength(50)
   @IsNotEmpty()
   email: string;
 
   @IsString()
+  @MinLength(10)
+  @MaxLength(15)
   @IsNotEmpty()
   telephone: string;
 
   @IsString()
+  @MinLength(3)
+  @MaxLength(50)
   @IsNotEmpty()
   address: string;
 
